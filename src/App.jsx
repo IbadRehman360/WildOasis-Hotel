0d04e4294281571327c82d124343ea545b5cb07d
IbadRehman360 <ibadhashim4@gmail.com>
2023-09-23 21:32:27 +0500

    made few addional change on main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import styled from "styled-components"
 
+const H1 = styled.h1`
+font-size: 30px;
+font-weight: bold;
+`;
 function App() {
   return (
     <div>
-
+      <H1>
+        THE WILD OASIS
+      </H1>
     </div>
   )
 }
