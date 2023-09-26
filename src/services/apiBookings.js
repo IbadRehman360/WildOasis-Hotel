1653220f511884a2fac881f12ce914c336ef7bc0
IbadRehman360 <ibadhashim4@gmail.com>
2023-09-26 09:19:04 +0500

    made alot of change on cabins component, added few folders for fetching and etc.

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -15,6 +15,7 @@ export async function getBooking(id) {
 
   return data;
 }
+
 // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
 export async function getBookingsAfterDate(date) {
   const { data, error } = await supabase
@@ -47,6 +48,7 @@ export async function getStaysAfterDate(date) {
 
   return data;
 }
+
 // Activity means that there is a check in or a check out today
 export async function getStaysTodayActivity() {
   const { data, error } = await supabase
@@ -56,6 +58,7 @@ export async function getStaysTodayActivity() {
       `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`
     )
     .order("created_at");
+
   // Equivalent to this. But by querying this, we only download the data we actually need, otherwise we would need ALL bookings ever created
   // (stay.status === 'unconfirmed' && isToday(new Date(stay.startDate))) ||
   // (stay.status === 'checked-in' && isToday(new Date(stay.endDate)))
